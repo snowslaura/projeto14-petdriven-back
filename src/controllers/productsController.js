42bec0b8c216a3381f11838bab00e54de12385f5
Leonardo Lucas <leolucasguedes@yahoo.com.br>
2022-05-12 19:29:37 -0300

    feature: getProducts

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -2,7 +2,7 @@ import db from "./../app/db.js"
 
 export async function getProducts(req, res) {
     const token = req.headers.authorization;
-    if (!token) return res.status(401).send({ message: "Token não encontrado" });
+   // if (!token) return res.status(401).send({ message: "Token não encontrado" });
   
     try {
       const products = await db.collection("products").find({}).toArray();
@@ -11,17 +11,4 @@ export async function getProducts(req, res) {
       res.status(401).send({ message: "Erro ao pegar produtos" });
     }
   }
-
-  export async function getCategory(req, res) {
-    const token = req.headers.authorization;
-    if (!token) return res.status(401).send({ message: "Token não encontrado" });
   
-    const { type } = req.params;
-  
-    try {
-      const category = await db.collection("products").find({ type }).toArray();
-      res.status(200).send(category);
-    } catch (err) {
-      res.status(401).send({ message: "Erro ao pegar produtos da categoria" });
-    }
-  }
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,12 +1,8 @@
 import { Router } from "express";
-import {
-  getCategory,
-  getProducts,
-} from "./../controllers/productsController.js";
+import { getProducts } from "./../controllers/productsController.js";
 
 const productsRouter = Router();
 
-productsRouter.get("/products", getProducts);
-productsRouter.get("/products/:type", getCategory);
+productsRouter.get("/home", getProducts);
 
 export default productsRouter;
