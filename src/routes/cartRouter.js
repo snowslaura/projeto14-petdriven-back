c1de08930ebf5f4d2fb992dcdf68090210a9d448
José Rodolpho <j.rodolpho29@hotmail.com>
2022-05-14 18:17:36 -0300

    update: general fix / error handling

--- src/routes/cartRouter.js
+++ src/routes/cartRouter.js
@@ -7,6 +7,6 @@ const cartRouter = Router()
 cartRouter.get("/cart", validateToken, findCart, getCart)
 cartRouter.delete("/cart/:id", deleteProduct)
 cartRouter.post("/cart/:id", addProduct)
-cartRouter.delete("/checkout", finalizePurchase)
+cartRouter.delete("/checkout", validateToken, findCart ,finalizePurchase)
 
 export default cartRouter
