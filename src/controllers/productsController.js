0b0a2deb0ae84a61b611540dbd8b5c34a9e35ef0
Leonardo Lucas <leolucasguedes@yahoo.com.br>
2022-05-14 17:12:54 -0300

    bugfix: token getProducts

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,9 +1,6 @@
 import db from "./../app/db.js"
 
 export async function getProducts(req, res) {
-    const token = req.headers.authorization;
-   // if (!token) return res.status(401).send({ message: "Token não encontrado" });
-  
     try {
       const products = await db.collection("products").find({}).toArray();
       res.status(200).send(products);
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
+import { validateToken } from "../middlewares/authMiddlewares.js"
 import { getProducts } from "./../controllers/productsController.js";
 
 const productsRouter = Router();
 
 productsRouter.get("/home", getProducts);
+
+//productsRouter.get("/home", validateToken, getProducts);
 
 export default productsRouter;
